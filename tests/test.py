35cd316d2eb2870d3fef36177ca285283de0b131
Trevor Stanhope <trevor@firebase>
2015-08-06 01:21:25 -0400

    Added some modded images

--- tests/test.py
+++ tests/test.py
@@ -9,13 +9,25 @@ print 'Generating matcher ...'
 matcher = Matcher()
 
 print 'Generating training dataset ...'
-for file in os.listdir(data_dir):
-    if file.endswith(data_type):
-        bgr = cv2.imread(os.path.join(data_dir, file))
-        phenotype = file.split('_')[0] # phenotype is start of name before '_'
+training_set = [
+    'V1_a.jpg',
+    'V1_b.jpg',
+    'V1_c.jpg',
+    'V1_d.jpg'
+]
+for f in training_set:
+    if f.endswith(data_type):
+        bgr = cv2.imread(os.path.join(data_dir, f))
+        phenotype = f.split('_')[0] # phenotype is start of name before '_'
+        ver = f.split('_')[1]
         obj = matcher.train(bgr, phenotype) 
-        print phenotype, obj
+        print phenotype, ver, obj
 
 print 'Classifying test images ...'
-res = matcher.classify(bgr)
-print res
+test_set = [
+    'V1_a3.jpg'
+]
+for f in test_set:
+    bgr = cv2.imread(os.path.join(data_dir, f))
+    res = matcher.classify(bgr)
+    print f, res
